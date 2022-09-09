81908d63683ead52338f5cae4d9cabdc8721aaea
aaronresnick <110271436+aaronresnick@users.noreply.github.com>
2022-09-09 18:58:08 -0400

    Update redirect-to-miw-site.js

--- redirect-to-miw-site.js
+++ redirect-to-miw-site.js
@@ -4,7 +4,7 @@ function getElementsByText(str, tag = 'span') {
   
   // returns an array of 2 elements, for some reason
   let span = getElementsByText('Open live site');
-  // get parent element of the span, i.e. the “Open live site” button, then get the href
-  let siteURL = span[1].closest(‘a’).href;
-  // redirect to the href
-  window.location.href=`${siteURL}`
+  // get parent element of the span, i.e. the “Open live site” button
+  let button = span[1].closest(‘a’;
+  // simulate button click
+ button.click();
